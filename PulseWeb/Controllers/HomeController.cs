a1874c90a3e05a97fc69b9a74875e59353357495
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2024-05-09 01:51:58 -0700

    Able to display data from all of the models in the home page.

--- PulseWeb/Controllers/HomeController.cs
+++ PulseWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PulseWeb.Models;
+using PulseWeb.Repository.IRepository;
 using System.Diagnostics;
 
 namespace PulseWeb.Controllers
@@ -7,15 +8,34 @@ namespace PulseWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IGoalRepository _goalRepository;
+        private readonly IToDoRepository _toDoRepository;
+        private readonly IBudgetRepository _budgetRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IGoalRepository goalRepository, IToDoRepository toDoRepository, IBudgetRepository budgetRepository)
         {
             _logger = logger;
+            _goalRepository = goalRepository;
+            _toDoRepository = toDoRepository;
+            _budgetRepository = budgetRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Fetch data from repositories
+            var goals = _goalRepository.GetAll();
+            var toDo = _toDoRepository.GetAll();
+            var budget = _budgetRepository.GetAll();
+
+            // Create a view model to hold the data
+            var viewModel = new HomeViewModel
+            {
+                Goals = goals,
+                ToDoItems = toDo,
+                BudgetItems = budget
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ PulseWeb/Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PulseWeb.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Goal>? Goals { get; set; }
+        public IEnumerable<ToDoItem>? ToDoItems { get; set; }
+        public IEnumerable<BudgetItem>? BudgetItems { get; set; }
+    }
+}
