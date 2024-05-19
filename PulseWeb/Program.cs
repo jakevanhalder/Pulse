7b48dbcc249334b3e154d97c53b22c5328dc61a5
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2024-05-19 02:13:38 -0700

    Starting to implement register and login capabilities.

--- PulseWeb/Program.cs
+++ PulseWeb/Program.cs
@@ -3,14 +3,18 @@ using PulseWeb.Data;
 using Microsoft.Extensions.DependencyInjection;
 using PulseWeb.Repository.IRepository;
 using PulseWeb.Repository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IGoalRepository, GoalRepository>();
 builder.Services.AddScoped<IToDoRepository, ToDoRepository>();
@@ -30,9 +34,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
