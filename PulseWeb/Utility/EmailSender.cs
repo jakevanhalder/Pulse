6e3a3ccc6a7e57be16ef939a9860fb5b3893ee07
jakevanhalder <ID+USERNAME@users.noreply.github.com>
2024-05-27 12:34:15 -0700

    Final commit?

--- PulseWeb/Utility/EmailSender.cs
+++ PulseWeb/Utility/EmailSender.cs
@@ -7,13 +7,6 @@ using System.Drawing.Printing;
 namespace PulseWeb.Utility
 {
     // If you're using this code to work on your own project you have to place your sendgrid api key in appsettings.json
-    // i.e
-    // "ConnectionStrings": {
-    //      "DefaultConnection": "Server=(localdb)\\MSSQLLocalDB;Database=Pulse;Trusted_Connection=True;TrustServerCertificate=True;"
-    //  },
-    //  "SendGrid": {
-    //      "SecretKey": "yourapikey"
-    //  }
 public class EmailSender : IEmailSender
     {
         public string SendGridSecret { get; set; }
