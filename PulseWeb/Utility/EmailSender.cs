ce01e990ede4da1f2b78fda8bb089328bc15c7f2
jakevanhalder <ID+USERNAME@users.noreply.github.com>
2024-05-27 01:02:01 -0700

    FInal commit.

--- PulseWeb/Utility/EmailSender.cs
+++ PulseWeb/Utility/EmailSender.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System.Drawing.Printing;
 
 namespace PulseWeb.Utility
 {
-    public class EmailSender : IEmailSender
+    // If you're using this code to work on your own project you have to place your sendgrid api key in appsettings.json
+    // i.e
+    // "ConnectionStrings": {
+    //      "DefaultConnection": "Server=(localdb)\\MSSQLLocalDB;Database=Pulse;Trusted_Connection=True;TrustServerCertificate=True;"
+    //  },
+    //  "SendGrid": {
+    //      "SecretKey": "yourapikey"
+    //  }
+public class EmailSender : IEmailSender
     {
         public string SendGridSecret { get; set; }
 
-        public EmailSender(IConfiguration _config) 
+        public EmailSender(IConfiguration _config)
         {
             SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
         }
@@ -17,12 +27,10 @@ namespace PulseWeb.Utility
         {
             // logic to send email
             var client = new SendGridClient(SendGridSecret);
-
-            var from = new EmailAddress("", "Pulse");
-
+            var from = new EmailAddress("no-reply@pulsetrackr.org", "Pulse");
             var to = new EmailAddress(email);
-            
             var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
             return client.SendEmailAsync(message);
         }
     }
